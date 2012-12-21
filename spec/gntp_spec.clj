937fdfbcfc6875e0e6b984c0eb1a03bd9e1a5fd2
Jonathan Grochowski <jongrocho@gmail.com>
2012-12-20 21:11:33 -0800

    Flatten request sequence in specs
    In order to make the `in?` spec helper function easier to use, I
    flattened the request sequence in the specs. The request was a sequence
    of double-newline separated sections, but this was superfluous. The
    flattened sequence means the `in?` function is much easier to use.

--- spec/gntp_spec.clj
+++ spec/gntp_spec.clj
@@ -54,7 +54,7 @@
   "Splits output-stream into blocks seperated by two carriage-return/newline
   pairs and splits each block by carriage-return/newline."
   []
-  (map split-lines (split (.toString output-stream) #"\r\n\r\n")))
+  (flatten (map split-lines (split (.toString output-stream) #"\r\n\r\n"))))
 
 (defmacro ^:private do-first [f & body]
   `(let [result# ~f]
@@ -78,19 +78,19 @@
     (it "connects on port 23053"
       (should= 23053 @socket-port))
     (it "does not send a password"
-      (should= "GNTP/1.0 REGISTER NONE" (first (first @request))))
+      (should= "GNTP/1.0 REGISTER NONE" (first @request)))
     (it "does not have an icon"
-      (should-not (some #(in? #"Application-Icon:" %) @request)))
+      (should-not (in? #"Application-Icon:" @request)))
     (it "has a machine name"
-      (should (some #(in? #"Origin-Machine-Name: \S+" %) @request)))
+      (should (in? #"Origin-Machine-Name: \S+" @request)))
     (it "has a software name"
-      (should (some #(in? #"Origin-Software-Name: \S+" %) @request)))
+      (should (in? #"Origin-Software-Name: \S+" @request)))
     (it "has a software version"
-      (should (some #(in? #"Origin-Software-Version: \S+" %) @request)))
+      (should (in? #"Origin-Software-Version: \S+" @request)))
     (it "has a platform name"
-      (should (some #(in? #"Origin-Platform-Name: \S+" %) @request)))
+      (should (in? #"Origin-Platform-Name: \S+" @request)))
     (it "has a platform version"
-      (should (some #(in? #"Origin-Platform-Version: \S+" %) @request))))
+      (should (in? #"Origin-Platform-Version: \S+" @request))))
 
   (describe "when given a host name"
     (with growler (make-growler default-name :host "example.com"))
@@ -110,7 +110,7 @@
     (before (@growler))
     (it "has a password"
       (should
-        (some #(in? #"GNTP/1.0 REGISTER NONE SHA512:\S+" %) @request))))
+        (in? #"GNTP/1.0 REGISTER NONE SHA512:\S+" @request))))
 
   (describe "when given an icon"
     (describe "as a url"
@@ -119,23 +119,23 @@
       (before (@growler))
       (it "has an icon url"
         (should
-          (some #(in? (str "Application-Icon: " (.toString icon-url)) %)
+          (in? (str "Application-Icon: " (.toString icon-url))
                 @request))))
     (describe "as a file"
       (with growler (make-growler default-name :icon icon-file))
       (with request (read-output))
       (before (@growler))
       (it "has an icon resource pointer"
-        (should (some #(in? #"Application-Icon: x-growl-resource://\S+" %)
+        (should (in? #"Application-Icon: x-growl-resource://\S+"
                       @request)))
       (it "has an identifier"
-        (should (some #(in? #"Identifier: \S+" %) @request)))
+        (should (in? #"Identifier: \S+" @request)))
       (it "has a length"
-        (should (some #(in? #"Length: \d+" %) @request)))
+        (should (in? #"Length: \d+" @request)))
       (it "has matching pointer and identifier"
-        (let [pointer-l (some #(in? #"Application-Icon: x-growl-resource://\S+" %) @request)
+        (let [pointer-l (in? #"Application-Icon: x-growl-resource://\S+" @request)
               pointer (second (re-matches #"Application-Icon: x-growl-resource://(\S+)" pointer-l))
-              identifier-l (some #(in? #"Identifier: \S+" %) @request)
+              identifier-l (in? #"Identifier: \S+" @request)
               identifier (second (re-matches #"Identifier: (\S+)" identifier-l))]
           (should= pointer identifier))))))
 
@@ -153,67 +153,63 @@
   (describe "with default parameters"
     (before (@growler :notify nil))
     (it "has a reasonable display name"
-      (should (some #(in? "Notification-Display-Name: notify" %) @request)))
+      (should (in? "Notification-Display-Name: notify" @request)))
     (it "is enabled"
-      (should (some #(in? "Notification-Enabled: true" %) @request)))
+      (should (in? "Notification-Enabled: true" @request)))
     (it "does not have an icon"
-      (should-not (some #(in? "Notification-Icon:" %) @request)))
+      (should-not (in? "Notification-Icon:" @request)))
     (it "has a machine name"
-      (should (some #(in? #"Origin-Machine-Name: \S+" %) @request)))
+      (should (in? #"Origin-Machine-Name: \S+" @request)))
     (it "has a software name"
-      (should (some #(in? #"Origin-Software-Name: \S+" %) @request)))
+      (should (in? #"Origin-Software-Name: \S+" @request)))
     (it "has a software version"
-      (should (some #(in? #"Origin-Software-Version: \S+" %) @request)))
+      (should (in? #"Origin-Software-Version: \S+" @request)))
     (it "has a platform name"
-      (should (some #(in? #"Origin-Platform-Name: \S+" %) @request)))
+      (should (in? #"Origin-Platform-Name: \S+" @request)))
     (it "has a platform version"
-      (should (some #(in? #"Origin-Platform-Version: \S+" %) @request))))
+      (should (in? #"Origin-Platform-Version: \S+" @request))))
 
   (describe "when given a name"
     (before (@growler :notify {:name "Notification"}))
     (it "has a display name"
       (should
-        (some #(in? "Notification-Display-Name: Notification" %) @request))))
+        (in? "Notification-Display-Name: Notification" @request))))
 
   (describe "when disabled"
     (before (@growler :notify {:enabled false}))
     (it "is not enabled"
-      (should (some #(in? "Notification-Enabled: false" %) @request))))
+      (should (in? "Notification-Enabled: false" @request))))
 
   (describe "when given an icon"
     (describe "as a url"
       (before (@growler :notify {:icon icon-url}))
       (it "has an icon url"
-        (should (some #(in? #"Notification-Icon: \S+" %) @request))))
+        (should (in? #"Notification-Icon: \S+" @request))))
     (describe "as a file"
       (before (@growler :notify {:icon icon-file}))
       (it "has an icon resource pointer"
-        (should (some #(in? #"Notification-Icon: x-growl-resource://\S+" %)
+        (should (in? #"Notification-Icon: x-growl-resource://\S+"
                       @request)))
       (it "has an identifier"
-        (should (some #(in? #"Identifier: \S+" %) @request)))
+        (should (in? #"Identifier: \S+" @request)))
       (it "has a length"
-        (should (some #(in? #"Length: \d+" %) @request)))
+        (should (in? #"Length: \d+" @request)))
       (it "has matching pointer and identifier"
-        (let [pointer-l (some #(in? #"Notification-Icon: x-growl-resource://\S+" %) @request)
+        (let [pointer-l (in? #"Notification-Icon: x-growl-resource://\S+" @request)
               pointer (second (re-matches #"Notification-Icon: x-growl-resource://(\S+)" pointer-l))
-              identifier-l (some #(in? #"Identifier: \S+" %) @request)
+              identifier-l (in? #"Identifier: \S+" @request)
               identifier (second (re-matches #"Identifier: (\S+)" identifier-l))]
           (should= pointer identifier)))))
 
   (describe "zero notifications"
     (before (@growler))
-    (it "has one section"
-      (should= 1 (count @request)))
     (it "has a zero notification count"
-      (should (some #(in? "Notifications-Count: 0" %) @request))))
+      (should (in? "Notifications-Count: 0" @request))))
 
   (describe "multiple notifications"
     (before (@growler :notify nil :notify2 nil :notify3 nil))
-    (it "has the correct number of sections"
-      (should= 4 (count @request)))
     (it "has the courrect notification count"
-      (should (some #(in? "Notifications-Count: 3" %) @request)))))
+      (should (in? "Notifications-Count: 3" @request)))))
 
 (describe "Sending notifications"
 
@@ -236,80 +232,80 @@
     (it "returns a UUID"
       (should= UUID (class @id)))
     (it "has a title"
-      (should (some #(in? "Notification-Title: Notification" %) @request)))
+      (should (in? "Notification-Title: Notification" @request)))
     (it "has an id"
-      (should (some #(in? #"Notification-ID: \S+" %) @request)))
+      (should (in? #"Notification-ID: \S+" @request)))
     (describe "coalescing id"
       (it "has one"
-        (should (some #(in? #"Notification-Coalescing-ID: \S+" %) @request)))
+        (should (in? #"Notification-Coalescing-ID: \S+" @request)))
       (it "matches the id"
-        (let [id-l (some #(in? #"Notification-ID: \S+" %) @request)
+        (let [id-l (in? #"Notification-ID: \S+" @request)
               id (second (re-matches #"Notification-ID: (\S+)" id-l))
-              coal-id-l (some #(in? #"Notification-Coalescing-ID: \S+" %) @request)
+              coal-id-l (in? #"Notification-Coalescing-ID: \S+" @request)
               coal-id (second (re-matches #"Notification-Coalescing-ID: (\S+)" coal-id-l))]
           (should= id coal-id))))
     (it "has no text"
-      (should (some #(in? "Notification-Text: " %) @request)))
+      (should (in? "Notification-Text: " @request)))
     (it "is not sticky"
-      (should (some #(in? "Notification-Sticky: false" %) @request)))
+      (should (in? "Notification-Sticky: false" @request)))
     (it "has normal priority"
-      (should (some #(in? "Notification-Priority: 0" %) @request)))
+      (should (in? "Notification-Priority: 0" @request)))
     (it "does not have an icon"
-      (should-not (some #(in? #"Notification-Icon: \S+" %) @request)))
+      (should-not (in? #"Notification-Icon: \S+" @request)))
     (it "does not have a callback"
-      (should-not (some #(in? #"Notification-Callback-\S+: .*" %) @request)))
+      (should-not (in? #"Notification-Callback-\S+: .*" @request)))
     (it "has a machine name"
-      (should (some #(in? #"Origin-Machine-Name: \S+" %) @request)))
+      (should (in? #"Origin-Machine-Name: \S+" @request)))
     (it "has a software name"
-      (should (some #(in? #"Origin-Software-Name: \S+" %) @request)))
+      (should (in? #"Origin-Software-Name: \S+" @request)))
     (it "has a software version"
-      (should (some #(in? #"Origin-Software-Version: \S+" %) @request)))
+      (should (in? #"Origin-Software-Version: \S+" @request)))
     (it "has a platform name"
-      (should (some #(in? #"Origin-Platform-Name: \S+" %) @request)))
+      (should (in? #"Origin-Platform-Name: \S+" @request)))
     (it "has a platform version"
-      (should (some #(in? #"Origin-Platform-Version: \S+" %) @request))))
+      (should (in? #"Origin-Platform-Version: \S+" @request))))
 
   (describe "when given text"
     (before ((:notify @notifiers) "Notification" :text "Notification text"))
     (it "has text"
       (should
-        (some #(in? "Notification-Text: Notification text" %) @request))))
+        (in? "Notification-Text: Notification text" @request))))
 
   (describe "when given a 'replaces' id"
     (with replaces (UUID/randomUUID))
     (before ((:notify @notifiers) "Notification" :replaces @replaces))
     (it "has a coalescing id"
       (should
-        (some #(in? (str "Notification-Coalescing-ID: " @replaces) %) @request))))
+        (in? (str "Notification-Coalescing-ID: " @replaces) @request))))
 
   (describe "when made sticky"
     (before ((:notify @notifiers) "Notification" :sticky true))
     (it "is sticky"
-      (should (some #(in? "Notification-Sticky: true" %) @request))))
+      (should (in? "Notification-Sticky: true" @request))))
 
   (describe "when given a priority"
     (before ((:notify @notifiers) "Notification" :priority 2))
     (it "has a priority"
-      (should (some #(in? "Notification-Priority: 2" %) @request))))
+      (should (in? "Notification-Priority: 2" @request))))
 
   (describe "when given an icon"
     (describe "as a url"
       (before ((:notify @notifiers) "Notification" :icon icon-url))
       (it "has an icon url"
-        (should (some #(in? #"Notification-Icon: \S+" %) @request))))
+        (should (in? #"Notification-Icon: \S+" @request))))
     (describe "as a file"
       (before ((:notify @notifiers) "Notification" :icon icon-file))
       (it "has an icon resource pointer"
-        (should (some #(in? #"Notification-Icon: x-growl-resource://\S+" %)
+        (should (in? #"Notification-Icon: x-growl-resource://\S+"
                       @request)))
       (it "has an identifier"
-        (should (some #(in? #"Identifier: \S+" %) @request)))
+        (should (in? #"Identifier: \S+" @request)))
       (it "has a length"
-        (should (some #(in? #"Length: \d+" %) @request)))
+        (should (in? #"Length: \d+" @request)))
       (it "has matching pointer and identifier"
-        (let [pointer-l (some #(in? #"Notification-Icon: x-growl-resource://\S+" %) @request)
+        (let [pointer-l (in? #"Notification-Icon: x-growl-resource://\S+" @request)
               pointer (second (re-matches #"Notification-Icon: x-growl-resource://(\S+)" pointer-l))
-              identifier-l (some #(in? #"Identifier: \S+" %) @request)
+              identifier-l (in? #"Identifier: \S+" @request)
               identifier (second (re-matches #"Identifier: (\S+)" identifier-l))]
           (should= pointer identifier)))))
 
@@ -317,7 +313,7 @@
     (describe "as a URL"
       (before ((:notify @notifiers) "Notification" :callback (as-url "http://example.com")))
       (it "has a URL callback"
-        (should (some #(in? "Notification-Callback-Target: http://example.com" %) @request))))
+        (should (in? "Notification-Callback-Target: http://example.com" @request))))
     (describe "as a map"
       (around [it]
         (let [connect-stub
@@ -342,9 +338,9 @@
                                                               :context "Context"
                                                               :type "Type"}))
       (it "has a callback context"
-        (should (some #(in? "Notification-Callback-Context: Context" %) @request)))
+        (should (in? "Notification-Callback-Context: Context" @request)))
       (it "has a callback context type"
-        (should (some #(in? "Notification-Callback-Context-Type: Type" %) @request)))
+        (should (in? "Notification-Callback-Context-Type: Type" @request)))
       (describe "populating the callback agent"
         (before (await-for 1000 @callback))
         (it "sets the app-name"
