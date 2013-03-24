73e47271288205145d5843bfaa9b7d75ef51e1d4
Jonathan Grochowski <jongrocho@gmail.com>
2013-03-24 15:32:17 -0700

    Treat VERSION as a URL and not a file
    Fixes #1
    
    Resources should be treated as URLs and not files so that they are
    loaded consistently between JAR files and source checkouts.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject gntp "0.5.0"
+(defproject gntp "0.5.1"
   :description "Clojure library for sending notifications over GNTP"
   :url "https://github.com/jgrocho/clj-gntp"
   :license {:name "Eclipse Public License"
